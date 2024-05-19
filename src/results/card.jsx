d9526f477754926958a2aba712d0db8dc95f2938
JASON <jasonfelipecruzluera@gmail.com>
2024-05-18 23:25:57 -0500

    data-consumption

--- src/results/card.jsx
+++ src/results/card.jsx
@@ -1,86 +1,63 @@
 import React from "react";
+import { useEffect, useState } from 'react'
 import "./card.css";
-import departamento from "./../images/departamento.jpg"
 import star from "./../images/star.png"
 
 export const Results = () => {
 
+
+    const [data, setData] = useState([])
+
+    async function getData() {
+        const rs = await fetch('./stays.json')
+        const rsJson = await rs.json()
+
+        const filteredData = rsJson.map(user => {
+
+            return {
+                id: user.id,
+                img: user.photo,
+                type_beds: user.type + " . " + user.beds + "beds",
+                rating: user.rating,
+                title: user.title
+            }
+
+        })
+
+        setData(filteredData)
+    }
+    useEffect(() => {
+        getData()
+    }, [])
+
     return (
         <>
-        
-        <div className="container">
-            <div className="grid">
-                <div className="card">
-                    <img src={departamento} alt="Stylish apartment in center of the city"/>
-                    <div className="info">
-                        <span className="super-host">SUPER HOST</span>
-                        <span className="type">Entire apartment . 2 beds</span>
-                        <div className="container_rating">
-                            <img src={star} alt="icon-star"/>
-                            <span className="rating">4.25</span>
+
+            <div className="container">
+                <div className="grid">
+                {data &&
+                    data.map(user =>
+                        <div>
+                        <div className="card" key={user.id}>
+                            <img src={user.img} alt="Stylish apartment in center of the city" />
+                            <div className="info">
+                                <span className="super-host">SUPER HOST</span>
+                                <span className="type">{user.type_beds}</span>
+                                <div className="container_rating">
+                                    <img src={star} alt="icon-star" />
+                                    <span className="rating">{user.rating}</span>
+                                </div>
+                            </div>
+                            <span className="description">{user.title}</span>
                         </div>
-                    </div>
-                    <span className="description">Stylish apartment in center of the city</span>
-                </div>
-                <div className="card">
-                    <img src={departamento} alt="Cozy, peaceful and private room with..."/>
-                    <div className="info">
-                        <span className="type">Private room</span>
-                        <div className="container_rating">
-                            <img src={star}alt="icon-star"/>
-                            <span className="rating">4.25</span>
                         </div>
 
-                    </div>
-                    <span className="description">Cozy, peaceful and private room with...</span>
-                </div>
-                <div className="card">
-                    <img src={departamento} alt="Modern House in a remote area"/>
-                    <div className="info">
-                        <span className="type">Entire house</span>
-                        <div className="container_rating">
-                            <img src={star} alt="icon-star"/>
-                            <span className="rating">4.25</span>
-                        </div>
-                    </div>
-                    <span className="description">Modern House in a remote area</span>
-                </div>
-                <div className="card">
-                    <img src={departamento} alt="Stylish room in design district"/>
-                    <div className="info">
-                        <div className="super-host">SUPER HOST</div>
-                        <span className="type">Entire apartment . 2 beds</span>
-                        <div className="container_rating">
-                            <img src={star} alt="icon-star"/>
-                            <span className="rating">4.25</span>
-                        </div>
-                    </div>
-                    <span className="description">Stylish room in design district</span>
-                </div>
-                <div className="card">
-                    <img src={departamento} alt="Modern apartment close to nature"/>
-                    <div className="info">
-                        <span className="type">Private room</span>
-                        <div className="container_rating">
-                            <img src={star} alt="icon-star"/>
-                            <span className="rating">4.25</span>
-                        </div>
-                    </div>
-                    <span className="description">Modern apartment close to nature</span>
-                </div>
-                <div className="card">
-                    <img src={departamento} alt="House in a remote area"/>
-                    <div className="info">
-                        <span className="type">Entire house</span>
-                        <div className="container_rating">
-                            <img src={star} alt="icon-star"/>
-                            <span className="rating">4.25</span>
-                        </div>
-                    </div>
-                    <span className="description">House in a remote area</span>
+                    )
+                }
+
                 </div>
             </div>
-        </div>
+
         </>
     )
 }
--- src/results/header_results/index.jsx
+++ src/results/header_results/index.jsx
@@ -1,20 +1,43 @@
 import React from "react";
+import { useEffect, useState } from 'react'
 import "./styles.css";
 
 
 export const HeaderResults = () => {
 
-    return (
-        <>
-            <div class="header_results">
-                <div>
-                    <span class="text_city">Stays in "Finland"</span>
-                </div>
+    const [data, setData] = useState([])
+    const [country, setCountry] = useState('');
 
+    async function getData() {
+        const rs = await fetch('./stays.json')
+        const rsJson = await rs.json()
+
+        setData(rsJson.results)
+
+        if (rsJson.length > 0) {
+            setCountry(rsJson[0].country);
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+
+    return (
+
+        <>
+            <div className="header_results">
                 <div>
-                    <span class="amount">"12+"stays</span>
+                    <span className="text_city">Stays in {country}</span>
+                </div>
+                <div>
+                    <span className="amount">All stays</span>
                 </div>
             </div>
+
+ 
         </>
+
     )
 }
